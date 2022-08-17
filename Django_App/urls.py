7bef9c66d269a92e0a1c633155d4000eeafd048e
zexin bai <517291467@qq.com>
2022-08-17 15:03:46 +0800

    write my first page

--- Django_App/urls.py
+++ Django_App/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
+    #path('game/',include('game.urls')), #如果只有一个app可以用下面的写法
+    path('',include('game.urls')),
     path('admin/', admin.site.urls),
 ]
